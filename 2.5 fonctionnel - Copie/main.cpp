e15408a384aab951ab42325e8b075309fc0b4faa
SkillupandNIK <36380528+SkillupandNIK@users.noreply.github.com>
2018-04-05 17:04:12 +0200

    Add files via upload

---
+++ 2.5 fonctionnel - Copie/main.cpp
@@ -0,0 +1,39 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+int main()
+{
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+
+    /// Un exemple de graphe
+    Graph g;
+    g.make_example();
+
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        if (key[KEY_SPACE])
+            g.test_remove_vertex(1);
+
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update();
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+    }
+
+    grman::fermer_allegro();
+
+    return 0;
+}
+END_OF_MAIN();
+
+
